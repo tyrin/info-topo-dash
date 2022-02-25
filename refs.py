fb366f4d09c807e37ff567528eda32fa9507c6ff
Tyrin Avery <tavery@salesforce.com>
2022-02-25 10:51:17 -0500

    Cleaning up the directory and adding arrows to the viz.

--- refs.py
+++ refs.py
@@ -6,73 +6,105 @@ import sys
 import pandas as pd
 
 def vizrender(ref, domain, physics, search, term):
-    # set the network options
-    ccx_net = Network(height='750px', width='750', bgcolor='white', font_color='blue', heading="")
+	ccx_net = Network(height='750px', width='100%', bgcolor='white', font_color='blue', heading=filename)
 
-    #read inputfile
-    df = pd.read_csv("https://raw.githubusercontent.com/tyrin/info-topo-dash/master/data.csv")
-    #set outputfile
-    # filter by relationship and domain
-    if ref == 'all' and 'all' in domain:
-        dff = df
+	# set the network options
+	ccx_net.set_options('''
+	var options = {
+		"configure": {
+			"enabled": false
+		},
+		"edges": {
+		"arrows": {
+			"to": {
+			"enabled": true
+			}
+		},
+		"color": {
+			"inherit": true
+		},
+		"smooth": false
+		},
+		"interaction": {
+		"hideEdgesOnDrag": true
+		},
+		"physics": {
+		"barnesHut": {
+			"gravitationalConstant": -80000,
+			"springLength": 135,
+			"springConstant": 0.001,
+			"damping": 0.42,
+			"avoidOverlap": 0.75
+		},
+		"minVelocity": 0.75
+		}
+	}
+	''')
 
-    elif  ref=='all':
-        dff = df.loc[df['Group'].isin(domain)]
+	#read inputfile
+	df = pd.read_csv("https://raw.githubusercontent.com/tyrin/info-topo-dash/master/data/data.csv")
+	#set outputfile
+	# filter by relationship and domain
+	if ref == 'all' and 'all' in domain:
+		dff = df
 
-    elif domain=='all':
-        dff = df.loc[df['Ref'] == ref]
+	elif	ref=='all':
+		dff = df.loc[df['Group'].isin(domain)]
 
-    else:
-        dff = df.loc[(df['Group'].isin(domain)) & (df['Ref'] == ref)]
+	elif domain=='all':
+		dff = df.loc[df['Ref'] == ref]
+
+	else:
+		dff = df.loc[(df['Group'].isin(domain)) & (df['Ref'] == ref)]
 # keyword filtering
-    if term == 'no':
-        dfff = dff
+	if term == 'no':
+		dfff = dff
 
-    elif  search=='label':
-        dfff = dff.loc[(df['Label'].str.contains(term)) | (df['TargetLabel'].str.contains(term))]
+	elif	search=='label':
+		dfff = dff.loc[(df['Label'].str.contains(term)) | (df['TargetLabel'].str.contains(term))]
 
-    else:
-        dfff = dff.loc[(df['Source'].str.contains(term)) | (df['Target'].str.contains(term))]
+	else:
+		dfff = dff.loc[(df['Source'].str.contains(term)) | (df['Target'].str.contains(term))]
 
-    sources = dfff['Source']
-    targets = dfff['Target']
-    weights = dfff['Weight']
-    refs = dfff['Ref']
-    groups = dfff['Group']
-    colors = dfff['Color']
-    labels = dfff['Label']
-    tgtgroup = dfff['TargetGroup']
-    tgtcolors = dfff['TargetColor']
-    tgtlabels = dfff['TargetLabel']
+	sources = dfff['Source']
+	targets = dfff['Target']
+	weights = dfff['Weight']
+	refs = dfff['Ref']
+	groups = dfff['Group']
+	colors = dfff['Color']
+	labels = dfff['Label']
+	tgtgroup = dfff['TargetGroup']
+	tgtcolors = dfff['TargetColor']
+	tgtlabels = dfff['TargetLabel']
 
-    edge_data = zip(sources, targets, weights, refs, groups, colors, labels, tgtgroup, tgtcolors, tgtlabels)
+	edge_data = zip(sources, targets, weights, refs, groups, colors, labels, tgtgroup, tgtcolors, tgtlabels)
 
-    for e in edge_data:
-        src = e[0]
-        tgt = e[1]
-        w = e[2]
-        rt = e[3]
-        grp = e[4]
-        clr = e[5]
-        lbl = e[6]
-        tgtgrp = e[7]
-        tgtclr = e[8]
-        tgtlbl = e[9]
+	for e in edge_data:
+		src = e[0]
+		tgt = e[1]
+		w = e[2]
+		rt = e[3]
+		grp = e[4]
+		clr = e[5]
+		lbl = e[6]
+		tgtgrp = e[7]
+		tgtclr = e[8]
+		tgtlbl = e[9]
 
-    # node_id, label, named args
-        ccx_net.add_node(src, lbl, title=src, color=clr)
-        ccx_net.add_node(tgt, tgtlbl, title=tgt, color=tgtclr)
-        ccx_net.add_edge(src, tgt, value=w)
+	# node_id, label, named args
+		ccx_net.add_node(src, lbl, title=src, color=clr)
+		ccx_net.add_node(tgt, tgtlbl, title=tgt, color=tgtclr)
+		ccx_net.add_edge(src, tgt, value=w)
 
-    neighbor_map = ccx_net.get_adj_list()
+	neighbor_map = ccx_net.get_adj_list()
 
-    # add neighbor data to node hover data
-    for node in ccx_net.nodes:
-        node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
-        node['value'] = len(neighbor_map[node['id']])
-    if physics:
-        ccx_net.show_buttons(filter_=['physics'])
-        ccx_net.show("data.html")
-    else:
-        ccx_net.show("data.html")
-    #display(HTML(outputfile))
+	# add neighbor data to node hover data
+	for node in ccx_net.nodes:
+		node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
+		node['value'] = len(neighbor_map[node['id']])
+	if physics:
+		ccx_net.show_buttons(filter_=['physics'])
+		ccx_net.show("data.html")
+	else:
+		ccx_net.show("data.html")
+	#display(HTML(outputfile))
